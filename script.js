234c6cca0952cd364d62db5720681e05fde04f80
FacuECejas <facuemicejas@gmail.com>
2025-02-15 20:11:25 -0300

    feat: buttonHandling function

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 
-let firstOperand;
-let secondOperand;
+let firstOperand = 0;
+let secondOperand = null;
 let operator = "";
 
 const add = function(a, b){
@@ -32,4 +32,57 @@ const operate = function (firstOperand, operator, secondOperand){
         default:
             return;
     }
+}
+
+const display = document.querySelector(".display");
+display.textContent = firstOperand;
+
+const buttons = document.querySelectorAll("button");
+buttons.forEach((btn)=>{
+    btn.addEventListener('click', buttonHandling);
+})
+
+function buttonHandling(event) {
+    let btnKey = event.target.textContent;
+
+    if(btnKey === "Clear") {
+        handleClear();
+    } else if (btnKey === "Del") {
+        handleDelete();
+    } else if (btnKey === ".") {
+        handleDecimal();
+    } else if (btnKey === "=") {
+        handleEqual();
+    } else if ("+-*/".includes(btnKey)) {
+        handleOperator();
+    } else {
+        handleNumber();
+    }
+
+}
+
+function handleClear() {
+    return;
+}
+
+/*To implement later */
+function handleDelete() {
+    return;
+}
+
+/*To implement later */
+function handleDecimal() {
+    return;
+}
+
+function handleEqual() {
+    return;
+}
+
+function handleOperator() {
+    return;
+}
+
+function handleNumber() {
+    return;
 }
